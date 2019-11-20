48e7d54d82dd9d9865c948553f3afaaa05500b1c
vicnatacu <vicki@mvbmb.com>
2019-11-20 11:54:41 +1000

    Updates

---
+++ precache-manifest.16d10d229f78f5ccaf9318f367e358c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bde2ccc66d0c528c1a335a54c2f4a40",
+    "url": "/blog-client/index.html"
+  },
+  {
+    "revision": "7e23c8f691d4fb0e2c62",
+    "url": "/blog-client/static/css/2.46f0bf41.chunk.css"
+  },
+  {
+    "revision": "7e23c8f691d4fb0e2c62",
+    "url": "/blog-client/static/js/2.0a48bea9.chunk.js"
+  },
+  {
+    "revision": "88f98ab6c005ec915dd1",
+    "url": "/blog-client/static/js/main.151672c6.chunk.js"
+  },
+  {
+    "revision": "0a2fb8f7e4203d6b14ed",
+    "url": "/blog-client/static/js/runtime-main.2689d353.js"
+  }
+]);
